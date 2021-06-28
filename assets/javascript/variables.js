431c11c63f8cd96323d3362382a41a52473d4126
Nicole LaMonica <nicole.lamonica@libertymutual.com>
2021-06-28 13:37:25 -0500

    update variable js file with numbers

--- assets/javascript/variables.js
+++ assets/javascript/variables.js
@@ -14,3 +14,23 @@ account2 = "def2";
 console.log("2 let account1 " + account1);
 console.log("2 const accountMain " + accountMain);
 console.log("2 var account2 " + account2);
+
+let number1 = 123;
+console.log("1. number1 = " + number1);
+
+number1 += 15;
+console.log("2. number1 = " + number1);
+
+number1 -= 8;
+console.log("3. number1 = " + number1);
+
+let avg = (26 + 44 + 72 +86 +112) / 5;
+console.log("4. avg = " + avg);
+
+let theNumber = 14;
+console.log("5a. " + theNumber++);
+
+theNumber = 14;
+console.log("5b. " + ++theNumber);
+
+console.log("6. " + typeof theNumber);
