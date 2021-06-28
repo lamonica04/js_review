21ef3bcc7d0bd50ddcc19db2ba23c444ec4f069e
Nicole LaMonica <nicole.lamonica@libertymutual.com>
2021-06-28 11:19:51 -0500

    update variable js file

--- assets/javascript/variables.js
+++ assets/javascript/variables.js
@@ -1,1 +1,16 @@
 console.log("variables.js");
+
+let account1 = "abc";
+const accountMain = "Main Account";
+var account2 = "def";
+
+console.log("let account1 " + account1);
+console.log("const accountMain " + accountMain);
+console.log("var account2 " + account2);
+
+account1 = "abc1";
+account2 = "def2";
+
+console.log("2 let account1 " + account1);
+console.log("2 const accountMain " + accountMain);
+console.log("2 var account2 " + account2);
